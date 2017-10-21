81f1d1d08d93f4906220e7cb7f23fc75d361dd3d
Luke Pafford <lukepafford@gmail.com>
2017-10-20 23:01:18 -0700

    Removed all host variables as they were oudated

--- host_vars/bash/fix-ip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for file in $(find /home/lukepafford/IT/git/lukepafford-ansible/ansible/host_vars -type f); do
-	
-	CORRECT=$(awk '{if ($0 ~ "^ipv4") {print substr($0, 1, length($0)-1);}}' $file | awk '{ print $0"/24'\''"}')
-
-	sed -i -e "s|^ipv4.*[^2][^4]'$|$CORRECT|" $file
-done
